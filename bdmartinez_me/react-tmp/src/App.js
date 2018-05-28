09a55ede57b85ec9e23d46474de32c4dc6147f23
BDMartinez <BDMartinez@Live.com>
2018-05-27 22:59:29 -0400

    Commented

--- bdmartinez_me/react-tmp/src/App.js
+++ bdmartinez_me/react-tmp/src/App.js
@@ -1,14 +1,21 @@
+// Dependencies
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect
 } from "react-router-dom";
+import {
+  Segment,
+  Divider
+} from "semantic-ui-react";
 import Header from "./components/Header";
 import MainDisplay from "./components/MainDisplay"
 import logo from './logo.svg';
 import './App.css';
 
+
+// The App component will act as the router between displays
 class App extends Component {
   render() {
     return (
--- bdmartinez_me/react-tmp/src/components/MainDisplay/index.js
+++ bdmartinez_me/react-tmp/src/components/MainDisplay/index.js
@@ -1,7 +1,9 @@
+// Dependencies
 import React, {
   Component
 } from "react";
 
+// Create a component for the MainDisplay and export it 
 export default class MainDisplay extends Component {
   render() {
     return (
