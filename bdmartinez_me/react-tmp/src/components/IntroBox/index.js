a43a55d00c364daaeda8fad01ff60ba1c5e79480
BDMartinez <BDMartinez@Live.com>
2018-05-31 12:30:42 -0400

    eslint added, and introbox component boilerplate laid

---
+++ bdmartinez_me/react-tmp/src/components/IntroBox/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default const IntroBox = {
+  render() {
+    return(
+      <div>
+        <p>The Intro Box</p>
+        <p>Arbitror nam labore appellat eu do nisi incididunt excepteur id litteris qui
+        quae laboris se legam litteris sed veniam magna. Ea litteris ab vidisse nam
+        laborum multos sunt an enim id nisi a incurreret ad summis, ut et fugiat
+        doctrina. Fore voluptate nam mandaremus quo hic cupidatat se ullamco, fugiat
+        ingeniis hic eram aute, possumus sint export nostrud labore.</p>
+      </div>
+    )
+  }
+}
